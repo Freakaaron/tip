fed893e9bbc62f722fe1d0959a8aaa11ccc68da9
Freakaaron <arpitindersingh805@gmail.com>
2020-10-03 18:32:07 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Arpit Inder Singh on 03/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
